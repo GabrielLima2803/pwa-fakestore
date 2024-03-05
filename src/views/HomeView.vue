37401e425459612d8d8e049cf90ce9db665f35bc
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-05 19:41:47 -0300

    Fix IsMobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,15 @@
 import ListagemProdutos from '@/components/ListagemProdutos.vue'
 import MenuSuperior from '@/components/MenuSuperior.vue';
 import RodaPe from '@/components/RodaPe.vue';
+import RodaPeMobile from '@/components/RodaPeMobile.vue';
+import { useScreen } from '@/composables/screen';
 
+const {isMobile} = useScreen()
 </script>
 
 <template>
   <MenuSuperior />
   <ListagemProdutos />
-  <RodaPe/> 
+  <RodaPe v-if="!isMobile"/> 
+  <RodaPeMobile v-else/>
 </template>
