f9e8952a71435dec78a493ab953e8de993b11d74
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-13 13:17:39 -0300

    FIx RodaPe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <script setup>
 import ListagemProdutos from '@/components/ListagemProdutos.vue'
-import RodaPe from '@/components/RodaPe.vue';
-import RodaPeMobile from '@/components/RodaPeMobile.vue';
-import { useScreen } from '@/composables/screen';
+// import RodaPe from '@/components/RodaPe.vue';
+// import RodaPeMobile from '@/components/RodaPeMobile.vue';
+// import { useScreen } from '@/composables/screen';
 
-const {isMobile} = useScreen()
+// const {isMobile} = useScreen()
 </script>
 
 <template>
   <div class="content-wrapper">
     <ListagemProdutos />
-    <RodaPe v-if="!isMobile"/> 
-    <RodaPeMobile v-else class="mobile"/>
+    <!-- <RodaPe v-if="!isMobile"/> 
+    <RodaPeMobile v-else class="mobile"/> -->
   </div>
 </template>
 
