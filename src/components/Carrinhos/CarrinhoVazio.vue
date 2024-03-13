425f0f21d1b2b9a08c3e0b825c132a709cbeb634
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-13 13:17:31 -0300

    Criação do Component CarrinhoVazio e da View Carrinho

---
+++ src/components/Carrinhos/CarrinhoVazio.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Cart from 'vue-material-design-icons/Cart.vue';
+
+</script>
+
+
+<template>
+    <div class="sacolaVazia">
+    <!-- <img src="@/assets/img/carrinho/sacola.png" alt="" class="imgSacola"> -->
+    <Cart />
+    <h1>Sua sacola está vazia</h1>
+    <p>Você não adicionou nenhum item à sua sacola</p>
+    <router-link to="/"><button class="continuarComprando">Continuar comprando</button> </router-link>
+ </div>
+ </template>
+ 
+ <style scooped>
+ 
+ .continuarComprando{
+    background-color: #e74c3c;
+    color: white;
+    border: none;
+    width: 180px;
+    height: 40px;
+ }
+ .imgSacola{
+    width: 137px ;
+    height: 133.81px;
+    margin: 60px;
+ }
+ h1{
+    text-decoration-line: underline;
+    text-decoration-thickness: 2px;  
+ }
+ .sacolaVazia{
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     margin-top: 100px;
+ }
+ button{
+   border: none;
+   background-color: white;
+ }
+ 
+ 
+ </style>
---
+++ src/views/CarrinhoView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import CarrinhoVazio from "@/components/Carrinhos/CarrinhoVazio.vue"
+
+</script>
+
+<template>
+    <div>
+        <CarrinhoVazio/>
+    </div>
+</template>
+
+<style scoped>
+</style>
